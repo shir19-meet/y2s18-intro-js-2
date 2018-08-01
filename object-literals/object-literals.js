3ab7cefbab5c376e9123c6b8733c5d634e34d377
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:15:00 +0300

    objects

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me ={
+	firstName: "Shir",
+	lastName: "Shitrit",
+	favoriteFoods: ["fries" , "pizza" , "hamburger"],
+	age: 16,
+	picture: "https://www.taylorguitars.com/sites/default/files/styles/guitar_detail_vertical/public/responsive-guitar-detail/Taylor-214ce-blk-fr-2012.png?itok=V-51b7YZ"
+} 
+ var b = JSON.stringify(me)
+document.write(b)
