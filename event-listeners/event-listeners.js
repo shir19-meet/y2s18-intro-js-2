1c83847cf25d3bc0eac3664f3c830dae5ec6a5b7
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 17:24:24 +0300

    changes

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,18 @@
 // YOUR TASK: Add more pictures!
 var pictures = ['./imgs/dog.jpg'];
 var currentIndex = 0;
-
+ var pictures = document.getElementsByTagName ("img")[0];
+ pictures.addEventListener("click",function (event) {
+ 	alert("You clicked me!")
+ }
+)
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  
 
   // YOUR TASK: Finish this function!
 }
